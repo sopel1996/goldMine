707ffb8f05da6eacd174acac777b1cb325390d6a
sopel1996 <sopel@live.ru>
2020-12-12 02:04:31 +0300

    главная почти готова

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ const stone = document.querySelectorAll('.stoneTitle');
 const footerNav = document.querySelector('.footerMenuSocialLang').children[0];
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.cancel');
+const signUp = document.querySelector('.signUp');
+const popUpDesk = document.querySelectorAll('.popup');
 
 const popUp = evt => {
-    evt.preventDefault();
+    // evt.preventDefault();
 
     const target = evt.target;
 
@@ -21,8 +23,17 @@ const popUp = evt => {
     if (target.textContent.toLowerCase()=="помощь") {
         modal.classList.add('modalActive');
     }
+    if (target.textContent.toLowerCase()=="регистрация") {
+        modal.classList.add('modalActive');
+        popUpDesk[1].classList.add('modalSignUp');
+        popUpDesk[0].classList.add('modalSignUp');
+        closeModal.classList.add('closeModalSignUp');
+    }
     if (target.classList.contains('cancel') || target.classList.contains('modal')){
         modal.classList.remove('modalActive');
+        popUpDesk[1].classList.remove('modalSignUp');
+        popUpDesk[0].classList.remove('modalSignUp');
+        closeModal.classList.remove('closeModalSignUp');
     }
 }
 
@@ -33,3 +44,4 @@ for (var i=0; i<stone.length; i++){
 footerNav.addEventListener('click', popUp);
 closeModal.addEventListener('click', popUp);
 modal.addEventListener('click', popUp);
+signUp.addEventListener('click', popUp);
