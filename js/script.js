24a55116967b822468b8d13de47518f4597e73e9
sopel1996 <sopel@live.ru>
2020-12-12 15:46:24 +0300

    главная закончена

--- js/script.js
+++ js/script.js
@@ -5,35 +5,98 @@ const closeModal = document.querySelector('.cancel');
 const signUp = document.querySelector('.signUp');
 const popUpDesk = document.querySelectorAll('.popup');
 
+const signUpPopUp= document.querySelector(".popupSignUp");
+const newsPopUp= document.querySelector(".popupNews");
+const aboutUsPopUp= document.querySelector(".popupAboutUs");
+const contactsPopUp= document.querySelector(".popupContacts");
+const helpPopUp= document.querySelector(".popupHelp");
+const forgotPassContainer= document.querySelector(".forgotPassContainer");
+
+const forgotPass= document.querySelector(".forgotPass");
+
 const popUp = evt => {
     // evt.preventDefault();
 
     const target = evt.target;
 
-    
+    if (target.textContent.toLowerCase()=="забыли пароль?"){
+        modal.classList.add('modalActive');
+        popUpDesk[1].classList.add('modalSignUp');
+        popUpDesk[0].classList.add('modalSignUp');
+        closeModal.classList.add('closeModalSignUp');
+
+        forgotPassContainer.classList.remove('hide');
+        signUpPopUp.classList.add('hide');
+        newsPopUp.classList.add('hide');
+        aboutUsPopUp.classList.add('hide');
+        contactsPopUp.classList.add('hide');
+        helpPopUp.classList.add('hide');
+    }
     if (target.textContent.toLowerCase()=="новости") {
         modal.classList.add('modalActive');
+
+        forgotPassContainer.classList.add('hide');
+        signUpPopUp.classList.add('hide');
+        newsPopUp.classList.remove('hide');
+        aboutUsPopUp.classList.add('hide');
+        contactsPopUp.classList.add('hide');
+        helpPopUp.classList.add('hide');
     }
     if (target.textContent.toLowerCase()=="о нас") {
         modal.classList.add('modalActive');
+
+        forgotPassContainer.classList.add('hide');
+        signUpPopUp.classList.add('hide');
+        newsPopUp.classList.add('hide');
+        aboutUsPopUp.classList.remove('hide');
+        contactsPopUp.classList.add('hide');
+        helpPopUp.classList.add('hide');
     }
     if (target.textContent.toLowerCase()=="контакты") {
         modal.classList.add('modalActive');
+
+        forgotPassContainer.classList.add('hide');
+        signUpPopUp.classList.add('hide');
+        newsPopUp.classList.add('hide');
+        aboutUsPopUp.classList.add('hide');
+        contactsPopUp.classList.remove('hide');
+        helpPopUp.classList.add('hide');
     }
     if (target.textContent.toLowerCase()=="помощь") {
         modal.classList.add('modalActive');
+
+        forgotPassContainer.classList.add('hide');
+        signUpPopUp.classList.add('hide');
+        newsPopUp.classList.add('hide');
+        aboutUsPopUp.classList.add('hide');
+        contactsPopUp.classList.add('hide');
+        helpPopUp.classList.remove('hide');
     }
     if (target.textContent.toLowerCase()=="регистрация") {
         modal.classList.add('modalActive');
         popUpDesk[1].classList.add('modalSignUp');
         popUpDesk[0].classList.add('modalSignUp');
         closeModal.classList.add('closeModalSignUp');
+
+        forgotPassContainer.classList.add('hide');
+        signUpPopUp.classList.remove('hide');
+        newsPopUp.classList.add('hide');
+        aboutUsPopUp.classList.add('hide');
+        contactsPopUp.classList.add('hide');
+        helpPopUp.classList.add('hide');
     }
     if (target.classList.contains('cancel') || target.classList.contains('modal')){
         modal.classList.remove('modalActive');
         popUpDesk[1].classList.remove('modalSignUp');
         popUpDesk[0].classList.remove('modalSignUp');
         closeModal.classList.remove('closeModalSignUp');
+
+        forgotPassContainer.classList.remove('hide');
+        signUpPopUp.classList.remove('hide');
+        newsPopUp.classList.remove('hide');
+        aboutUsPopUp.classList.remove('hide');
+        contactsPopUp.classList.remove('hide');
+        helpPopUp.classList.remove('hide');
     }
 }
 
@@ -45,3 +108,4 @@ footerNav.addEventListener('click', popUp);
 closeModal.addEventListener('click', popUp);
 modal.addEventListener('click', popUp);
 signUp.addEventListener('click', popUp);
+forgotPass.addEventListener('click', popUp);
